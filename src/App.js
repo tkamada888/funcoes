82dcca2c51bf5a041f7e632e282c85b422a21050
Thiago <thiago.kamada@gmail.com>
2020-11-09 14:39:37 +0900

    Importando material ui e formulario cadastro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Fragment } from 'react';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 
 function App() {
   return (
+     <Fragment>
     <h1>Formulário de cadastro</h1>
+    <FormularioCadastro />
+    </Fragment>
   );
 }
 
